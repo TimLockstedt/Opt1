2a80ad09700b6ca8179d6494db48be44e1400a31
Tim Lockstedt <t.lockstedt@gmx.de>
2026-01-20 20:33:47 +0100

    sync pc

--- opt_lib.py
+++ opt_lib.py
@@ -54,6 +54,7 @@ class J_mittel():
         self.__n += 1
         return
 
+
 class data_class():
 	__data = []
 	__len = 0
@@ -64,8 +65,22 @@ class data_class():
 	__xIt = np.ndarray
 	__tIt = np.ndarray
 	__ItError = np.ndarray
+	__numx = np.ndarray	
+	__J = np.ndarray
+	__gesx = []
+	__gesnumx = []
+	__d = np.ndarray
 
-
+	def get_d(self):
+		return self.__d
+	def get_numx(self):
+		return self.__numx
+	def get_J(self):
+		return self.__J
+	def get_gesx(self):
+		return self.__gesx
+	def get_gesnumx(self):
+		return self.__gesnumx
 	def get_len(self):
 		return self.__len
 	def get_x(self):
@@ -129,15 +144,48 @@ class data_class():
 			self.__unterCounter += 1
 		return xVals, tVal
 
-	# def d(self, x:np.ndarray): # Abstandsfunktion
-	# 	d = np.ones_like(x) * np.inf # anfangs distanz auf sehr groß setzen
-	# 	order_mask = np.argsort(x) # nach i-ter achse sortieren, hier x achse -> 0
-	# 	x_ordered = x[order_mask]
-	# 	for i, x_i in enumerate(x_ordered):
-	# 		for j, x_j in enumerate(x_ordered[i+1:]): # nur Werte mit größerem Index (i+1) sind für den vergleich relevant, da nur diese vor dem Auto sind.
-	# 			dist = np.linalg.norm(x_j - x_i)
-	# 			d[i] = dist if d[i] > dist else d[i]
-	# 	return d
+	def J (self, x, dt, x_data):
+		result = 0
+		for i in range(len(x)):
+			result += dt * np.abs(x[i] - x_data[i])**2
+		return result
+	
+	def set_numx(self, dt, L, v_max, n_dataset = __counter, model = "log"):
+		self.next_dataset(n_dataset) # auf den gewollten Datensatz wechseln
+		self.LinInterplation(dt) # durchschnittlicher Fehler der Interpolation abfragen
+		self.switch_iteration() # Die interpolierten werte als zu berücksichtigende Daten setzen
+		self.__numx = np.zeros_like(self.get_x()) # Array definieren, in dem die numerisch bestimmten orte gespeichert werden
+		self.__J = np.zeros(self.get_t().shape[0] - 1)
+		for i in range(len(self.__J)+1):
+			x_soll, t_soll = self.getCurrentCoords() # Aktuelle ort, zeit punkte der autos ausgeben
+			if i != 0: # Falls wir schon einen Schritt gegangen sind
+				self.__J[i-1] = self.J(x, dt, x_soll) # Loss berechnen
+			else: # Falls der erste durchlauf
+				x, t = x_soll, t_soll # Startpositionen initialisieren
+			x, _ = self.single_step(x, v_max, L, dt, model) # x aktualisieren
+			self.__numx[:,i] = x
+			t += dt
+
+		self.__gesx.append(self.__x)
+		self.__gesnumx.append(self.__numx)
+		return	self.__numx
+
+	def f (self, d, v_max, model = "log"):
+		if model == "log":
+			return np.min([v_max * np.log(d), np.ones_like(d) * v_max], 0) # Limit v_max, wie im linearen modell
+		else: # lin
+			return v_max * (1 - 1/d)
+	
+	def single_step(self, x, v_max, L, dt, model):
+		N = len(x) # Number of initial values == number of vehicles in traffic
+		dx = np.zeros(N) # momente initialisieren
+		# Leader
+		# dx[-1] = v_max
+		dist = self.d(x)/L
+		# for i in range(N):
+		#         dx[i] = f(dist[i], v_max, model)
+		dx = self.f(dist, v_max, model)
+		return x + dt * dx, dx
 
 
 	def switch_iteration(self):
@@ -204,6 +252,17 @@ class data_class():
 		self.__tIt = zeit_int
 		# self.test_interpolation(dt)
 		return ort_int, zeit_int
+	
+	def d(self, x:np.ndarray): # Abstandsfunktion
+		d = np.ones_like(x) * np.inf # anfangs distanz auf sehr groß setzen
+		order_mask = np.argsort(x) # nach i-ter achse sortieren, hier x achse -> 0
+		x_ordered = x[order_mask]
+		for i in range(len(x_ordered)-1):
+			dist = np.linalg.norm(x_ordered[i] - x_ordered[i+1]) # Da die werte geordnet sind, muss nur die Distanz zum nächsten auto berücksichtigt werden
+			d[i] = dist if d[i] > dist else d[i]
+				
+		reordered_d = d[np.argsort(order_mask)]
+		return reordered_d
 
     
 def d(x:np.ndarray): # Abstandsfunktion
@@ -241,3 +300,55 @@ def test_interpolation(data, ort_int, zeit_int, dt):
 	if np.sum(avg_fehler >= 0.05) > 1:
 		print("ACHTUNG mindestenz einmal eine größere Abweichung als 5%")
 	return avg_fehler
+
+def p1(dt, data:data_class, v_max, d):
+	p1 = np.zeros_like(data.get_x()[0,:]) # anpassen
+	t_arr = np.flip(data.get_t())
+	for i, j in enumerate(t_arr):
+		if i == 0:
+			p = 0
+		else:
+			p = p + dt * 2*(data.get_numx()[0,-i]-data.get_x()[0,-i]) - dt * p * (v_max/d[-i])
+		p1[-i] = p
+	return p1
+
+def p2_N_1(dt, data:data_class, pi_1, d, n, v_max): # n tes auto
+	pi = np.zeros_like(data.get_x()[1:-1,:])[0] # anpassen
+	t_arr = np.flip(data.get_t())
+	for i, j in enumerate(t_arr):
+		if i == 0:
+			p = 0
+		else:
+			p = p + dt * 2*(data.get_numx()[n,-i]-data.get_x()[n,-i]) - dt * p * (v_max/d[-i]) + dt * pi_1[-i] * v_max/d[-i]
+		pi[-i] = p
+	return pi
+
+def pN(dt, data:data_class, pi_1, d, v_max): # n tes auto
+	pi = np.zeros_like(data.get_x()[1:-1,:])[0] # anpassen
+	t_arr = np.flip(data.get_t())
+	for i, j in enumerate(t_arr):
+		if i == 0:
+			p = 0
+		else:
+			p = p + dt * 2*(data.get_numx()[-1,-i]-data.get_x()[-1,-i]) + dt * pi_1[-i] * v_max/d[-i]
+		pi[-i] = p
+	return pi
+
+
+def pGes(dt, data:data_class, v_max):
+	darr = np.array([data.d(i) for i in data.get_x().T])
+	pges = np.zeros_like(data.get_x())
+	pges[0,:] = p1(dt, data, v_max, darr[:,0])
+	for n in range(1, pges.shape[0]-1):
+		pges[n,:] = p2_N_1(dt, data, pges[n-1,:], darr[:,n], n, v_max)
+	pges[n+1,:] = pN(dt, data, pges[n,:], darr[:,-1], v_max)
+	return pges
+
+def integral(arg, dt):
+    res = np.array([arg[i] * dt for i in range(arg.shape[0])])
+    return np.sum(res)
+
+def gradient(P_ges, d, v_max, L, dt):
+    sk0 = integral(P_ges[-1] + np.sum(np.array([P_ges[i] * np.log(d[i]/L) for i in range(P_ges.shape[0]-1)]), axis=0), dt)
+    sk1 = integral(np.sum(np.array([P_ges[i] * v_max/L for i in range(P_ges.shape[0]-1)]), axis=0), dt)
+    return (sk0, sk1)
