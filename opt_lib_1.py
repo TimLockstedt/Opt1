e89fb5278b8d775c9515e55c80e1e20d728451a1
Tim Lockstedt <T.Lockstedt@gmx.de>
2026-01-28 11:28:47 +0100

    gradient könnte tatsächlich richtig sein

--- opt_lib_1.py
+++ opt_lib_1.py
@@ -1,60 +1,6 @@
 from mat4py import loadmat
 import numpy as np
 
-# Kosten funktional
-def J (x, dt, x_data):
-    result = 0
-    for i in range(len(x)):
-        result += dt * np.abs(x[i] - x_data[i])**2
-    return result
-
-def f (d, v_max, model = "log"):
-    if model == "log":
-        return np.min([v_max * np.log(d), v_max], 1) # Limit v_max, wie im linearen modell
-    else: # lin
-        return v_max * (1 - 1/d)
-	
-def single_step(x, v_max, L, dt, model):
-    N = len(x) # Number of initial values == number of vehicles in traffic
-    dx = np.zeros(N) # momente initialisieren
-    # Leader
-    # dx[-1] = v_max
-    dist = d(x)/L
-    # for i in range(N):
-    #         dx[i] = f(dist[i], v_max, model)
-    dx = f(dist, v_max, model)
-    return x + dt * dx, dx
-    
-# Den mittlwert von J jederzeit ausgeben können:
-class J_mittel():
-    __val = 0
-    __n = 0
-    def set_val(self, x):
-        self.__val = x
-        return
-    def set_n(self, x):
-        self.__n = x
-        return
-    
-    def get_val(self):
-        return self.__val
-    def get_n(self):
-        return self.__n
-    
-    def get_res(self):
-        return self.__val/self.__n
-
-    def __init__(self, x):
-        self.__val = x
-        self.__n = 1
-        return
-    
-    def add(self, x):
-        self.__val += x
-        self.__n += 1
-        return
-
-
 class data_class():
 	__data = []
 	__len = 0
@@ -67,9 +13,9 @@ class data_class():
 	__ItError = np.ndarray
 	__numx = np.ndarray	
 	__J = np.ndarray
+	__d = np.ndarray
 	__gesx = []
 	__gesnumx = []
-	__d = np.ndarray
 	__gesJ = []
 
 	def get_gesJ(self):
@@ -111,9 +57,9 @@ class data_class():
 		self.__len = len(self.__data)
 		self.__counter = 0
 		self.setXYTFromData(self.__counter)
-		__gesx = []
-		__gesnumx = []
-		__gesJ = []
+		self.__gesx = []
+		self.__gesnumx = []
+		self.__gesJ = []
 		return
 	
 	def setXYTFromData(self, x):
@@ -155,19 +101,14 @@ class data_class():
 			self.__unterCounter += 1
 		return xVals, tVal
 
-	def J (self, x, dt, x_data):
-		result = 0
-		result = np.sum(dt * np.abs(x - x_data)**2, axis = )
-		for i in range(len(x)):
-			result += dt * np.linalg.norm(x[i] - x_data[i])**2 # .abs -> .linalg.norm
-		return result
+	def J(self, x, dt, x_data):
+		return np.sum(dt * np.power(np.linalg.norm(x - x_data, axis=0), 2))
 	
 	def set_numx(self, dt, L, v_max, n_dataset = __counter, model = "log"):
 		self.next_dataset(n_dataset) # auf den gewollten Datensatz wechseln
 		self.LinInterplation(dt) # durchschnittlicher Fehler der Interpolation abfragen
 		self.switch_iteration() # Die interpolierten werte als zu berücksichtigende Daten setzen
 		self.__numx = np.zeros_like(self.get_x()) # Array definieren, in dem die numerisch bestimmten orte gespeichert werden
-		self.__J = np.zeros(self.get_t().shape[0] - 1)
 		for i in range(self.get_x().shape[1]):
 			x_soll, t_soll = self.getCurrentCoords() # Aktuelle ort, zeit punkte der autos ausgeben
 			if i != 0: # Falls wir schon einen Schritt gegangen sind
@@ -184,7 +125,9 @@ class data_class():
 
 	def f (self, d, v_max, model = "log"):
 		if model == "log":
-			return np.min([v_max * np.log(d), np.ones_like(d) * v_max], 0) # Limit v_max, wie im linearen modell
+			v = v_max * np.log(d)
+			v[-1] = v_max		
+			return v# np.min([v_max * np.log(d), np.ones_like(d) * v_max], 0) # Limit v_max, wie im linearen modell
 		else: # lin
 			return v_max * (1 - 1/d)
 	
@@ -256,10 +199,10 @@ class data_class():
 		
 		ort = self.__x
 		zeit = self.__t
-		zeit_int = np.arange(zeit[0], zeit[-1], dt)
+		zeit_int = np.arange(0, zeit[-1] - zeit[0], dt)
 		ort_int = np.zeros((ort.shape[0],*zeit_int.shape))
 		for i, auto in enumerate(ort):
-			ort_int[i, :] = np.interp(zeit_int, zeit, auto)
+			ort_int[i, :] = np.interp(zeit_int, zeit - zeit[0], auto)
 		self.__xIt = ort_int
 		self.__tIt = zeit_int
 		# self.test_interpolation(dt)
@@ -276,92 +219,54 @@ class data_class():
 		reordered_d = d[np.argsort(order_mask)]
 		return reordered_d
 
-    
-def d(x:np.ndarray): # Abstandsfunktion
-	d = np.ones_like(x) * np.inf # anfangs distanz auf sehr groß setzen
-	order_mask = np.argsort(x) # nach i-ter achse sortieren, hier x achse -> 0
-	x_ordered = x[order_mask]
-	for i in range(len(x_ordered)-1):
-		dist = np.linalg.norm(x_ordered[i] - x_ordered[i+1]) # Da die werte geordnet sind, muss nur die Distanz zum nächsten auto berücksichtigt werden
-		d[i] = dist if d[i] > dist else d[i]
-			
-	reordered_d = d[np.argsort(order_mask)]
-	return reordered_d
+	def p1(self, dt, v_max, d):
+		p1 = np.zeros_like(self.get_x()[0,:]) # anpassen
+		t_arr = np.flip(self.get_t())
+		for i, j in enumerate(t_arr):
+			if i == 0:
+				p = 0
+			else:
+				p = p + dt * 2*(self.get_numx()[0,-i-1]-self.get_x()[0,-i-1]) - dt * p * (v_max/d[-i-1]) # [-i-1] - "-1" ist dafür, damit tatsächlich bei bspw. i = 1, -> t = T - dt, das vorletzte element ausgewählt wird (index [-2])
+			p1[-i-1] = p
+		return p1
 
+	def p2_N_1(self, dt, pi_1, d, n, v_max): # n tes auto
+		pi = np.zeros_like(self.get_x()[1:-1,:])[0] # anpassen
+		t_arr = np.flip(self.get_t())
+		for i, j in enumerate(t_arr):
+			if i == 0:
+				p = 0
+			else:
+				p = p + dt * 2*(self.get_numx()[n,-i-1]-self.get_x()[n,-i-1]) - dt * p * (v_max/d[-i-1]) + dt * pi_1[-i-1] * v_max/d[-i-1]
+			pi[-i-1] = p
+		return pi
 
-def LinInterplation(data, dt=0.001):
-	ort = data.get_x()
-	zeit = data.get_t()
-	zeit_int = np.arange(zeit[0], zeit[-1], dt)
-	ort_int = np.zeros((ort.shape[0], len(zeit_int)))
-	for i, auto in enumerate(ort):
-		steigung = (np.max(auto)-np.min(auto))/(np.max(zeit)-np.min(zeit))
-		ort_int[i,:] = steigung * np.arange(0, np.max(zeit)-np.min(zeit), dt) + np.min(auto)
-	return ort_int, zeit_int
+	def pN(self, dt, pi_1, d, v_max): # n tes auto
+		pi = np.zeros_like(self.get_x()[1:-1,:])[0] # anpassen
+		t_arr = np.flip(self.get_t())
+		for i, j in enumerate(t_arr):
+			if i == 0:
+				p = 0
+			else:
+				p = p + dt * 2*(self.get_numx()[-1,-i-1]-self.get_x()[-1,-i-1]) + dt * pi_1[-i-1] * v_max/d[-i-1]
+			pi[-i-1] = p
+		return pi
 
-def test_interpolation(data, ort_int, zeit_int, dt):
-	ort, zeit = data.get_x(), data.get_t()
-	rel_fehler = np.zeros(ort.shape[0])
-	for i, auto in enumerate(ort):
-		for x, t in zip(auto, zeit):
-			index = np.where((zeit_int >= (t - dt/2)) & (zeit_int <= (t + dt/2)))[0]
-			if len(index) != 0:
-				abs_fehler = np.abs(x-ort_int[i, index])
-				rel_fehler[i] = rel_fehler[i] + abs_fehler/np.abs(x)
-	avg_fehler = rel_fehler/len(zeit)
-	if np.sum(avg_fehler >= 0.05) > 1:
-		print("ACHTUNG mindestenz einmal eine größere Abweichung als 5%")
-	return avg_fehler
+	def pGes(self, dt, v_max):
+		darr = np.array([self.d(i) for i in self.get_x().T])
+		pges = np.zeros_like(self.get_x())
+		pges[0,:] = self.p1(dt, v_max, darr[:,0])
+		for n in range(1, pges.shape[0]-1):
+			pges[n,:] = self.p2_N_1(dt, pges[n-1,:], darr[:,n], n, v_max)
+		pges[n+1,:] = self.pN(dt, pges[n,:], darr[:,-1], v_max)
+		return pges
 
-def p1(dt, data:data_class, v_max, d):
-	p1 = np.zeros_like(data.get_x()[0,:]) # anpassen
-	t_arr = np.flip(data.get_t())
-	for i, j in enumerate(t_arr):
-		if i == 0:
-			p = 0
-		else:
-			p = p + dt * 2*np.abs(data.get_numx()[0,-i-1]-data.get_x()[0,-i-1]) - dt * p * (v_max/d[-i-1]) # [-i-1] - "-1" ist dafür, damit tatsächlich bei bspw. i = 1, -> t = T - dt, das vorletzte element ausgewählt wird (index [-2])
-		p1[-i-1] = p
-	return p1
+	def integral(self, arg, dt):
+		res = np.array([arg[i] * dt for i in range(arg.shape[0])])
+		return np.sum(res)
 
-def p2_N_1(dt, data:data_class, pi_1, d, n, v_max): # n tes auto
-	pi = np.zeros_like(data.get_x()[1:-1,:])[0] # anpassen
-	t_arr = np.flip(data.get_t())
-	for i, j in enumerate(t_arr):
-		if i == 0:
-			p = 0
-		else:
-			p = p + dt * 2*np.abs(data.get_numx()[n,-i-1]-data.get_x()[n,-i-1]) - dt * p * (v_max/d[-i-1]) + dt * pi_1[-i-1] * v_max/d[-i-1]
-		pi[-i-1] = p
-	return pi
-
-def pN(dt, data:data_class, pi_1, d, v_max): # n tes auto
-	pi = np.zeros_like(data.get_x()[1:-1,:])[0] # anpassen
-	t_arr = np.flip(data.get_t())
-	for i, j in enumerate(t_arr):
-		if i == 0:
-			p = 0
-		else:
-			p = p + dt * 2*np.abs(data.get_numx()[-1,-i-1]-data.get_x()[-1,-i-1]) + dt * pi_1[-i-1] * v_max/d[-i-1]
-		pi[-i-1] = p
-	return pi
-
-def pGes(dt, data:data_class, v_max):
-	darr = np.array([data.d(i) for i in data.get_x().T])
-	pges = np.zeros_like(data.get_x())
-	pges[0,:] = p1(dt, data, v_max, darr[:,0])
-	for n in range(1, pges.shape[0]-1):
-		pges[n,:] = p2_N_1(dt, data, pges[n-1,:], darr[:,n], n, v_max)
-	pges[n+1,:] = pN(dt, data, pges[n,:], darr[:,-1], v_max)
-	return pges
-
-def integral(arg, dt):
-    res = np.array([arg[i] * dt for i in range(arg.shape[0])])
-    return np.sum(res)
-
-def gradient(P_ges, d, v_max, L, dt):
-    skvmax = integral(P_ges[-1,:] + np.sum(np.array([P_ges[i,:] * np.log(d[i,:]/L) for i in range(P_ges.shape[0]-1)]), axis=0), dt)
-    skL = integral(np.sum(np.array([P_ges[i,:] * v_max/L for i in range(P_ges.shape[0]-1)]), axis=0), dt)
-    return (skvmax, skL)
-
-# f funktion, min, max weglassen, und update für xn auf vmax setzen
+	def gradient(self, P_ges, d, v_max, L, dt):
+		skvmax = self.integral(P_ges[-1,:] + np.sum(np.array([(P_ges[i,:] * (np.log(d[i,:] / L))) for i in range(P_ges.shape[0] - 1)]), axis=0), dt)
+		skL = self.integral(np.sum(np.array([P_ges[i,:] * v_max/L for i in range(P_ges.shape[0]-1)]), axis=0), dt)
+		# betrag = np.linalg.norm([skvmax, skL])
+		return (skvmax, skL)# (skvmax/betrag, skL/betrag)
